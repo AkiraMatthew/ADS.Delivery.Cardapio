b62defea16f4dbdb05e9f4c827d3cf356daa0881
AkiraShirou <87336992+AkiraShirou@users.noreply.github.com>
2024-10-11 14:16:18 -0300

    verificacao dos parametros string

--- ADS.Delivery/Models/Aplicacao/Classes/ADSDAPIParamInserirPrato.cs
+++ ADS.Delivery/Models/Aplicacao/Classes/ADSDAPIParamInserirPrato.cs
@@ -7,3 +7,25 @@ public class ADSDAPIParamInserirPrato
     public decimal PratoPreco { get; set; }
     public string CategoriaNome { get; set; }
 }
+
+internal static class ADSValidadorPratoParam
+{
+    internal static (bool Resultado, string? Mensagem) ValidarEntrada(ADSDAPIParamInserirPrato prato, ADSDAPIParamInserirCategoria categoria)
+    {
+        var validacaoNome = VerificacaoString(prato.PratoNome);
+        if(!validacaoNome.Resultado)
+            return validacaoNome;
+
+        var validacaoDescricao = VerificacaoString(prato.PratoDescricao);
+        if(!validacaoDescricao.Resultado) 
+            return validacaoDescricao;
+
+        static (bool Resultado, string? Mensagem) VerificacaoString(string? parametro)
+        {
+            if (string.IsNullOrWhiteSpace(parametro))
+                return (false, $"O campo {nameof(parametro)}");
+
+            return (true, null);
+        }
+    }
+}
