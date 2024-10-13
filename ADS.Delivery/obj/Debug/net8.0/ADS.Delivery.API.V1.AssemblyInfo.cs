02f1603286bab567e45e9b693d8fb95856db8545
AkiraShirou <87336992+AkiraShirou@users.noreply.github.com>
2024-10-13 13:47:51 -0300

    x

--- ADS.Delivery/obj/Debug/net8.0/ADS.Delivery.API.V1.AssemblyInfo.cs
+++ ADS.Delivery/obj/Debug/net8.0/ADS.Delivery.API.V1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ADS.Delivery.API.V1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c21086d77691f509ebd7e9c9b56da72cf137154")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ef4dd217a3fbb60639a9f8c5a3dcab405ffa3bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ADS.Delivery.API.V1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ADS.Delivery.API.V1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
