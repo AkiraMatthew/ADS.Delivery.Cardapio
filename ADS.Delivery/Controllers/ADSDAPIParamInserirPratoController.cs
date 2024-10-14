11ce95d039f5a7acddb713cee9f84d47d6268cae
AkiraShirou <87336992+AkiraShirou@users.noreply.github.com>
2024-10-14 09:53:36 -0300

    correçao de bugs

--- ADS.Delivery/Controllers/ADSDAPIParamInserirPratoController.cs
+++ ADS.Delivery/Controllers/ADSDAPIParamInserirPratoController.cs
@@ -20,15 +20,16 @@ public class ADSDAPIParamInserirPratoController(IADSDAplicacaoPratos _adsAplicac
         {
             // Passo 1.1: Validar se os parametros que chegaram sao validos(prato e categorias)
             //Este passo se passa antes da inserçao do material
-            var resultadoValidacaoDadosDeEntrada = parametroPrato.ValidarPropsDeEntradaPrato();
-            if (!resultadoValidacaoDadosDeEntrada.Resultado)
-                return BadRequest(resultadoValidacaoDadosDeEntrada.Mensagem);
-            // Passo 1.2 desenvolver método para validar os parametros de categoria
+            var resultadoValidacaoDadosDeEntradaPrato = parametroPrato.ValidarPropsDeEntradaPrato();
+            if (!resultadoValidacaoDadosDeEntradaPrato.Resultado)
+                return BadRequest(resultadoValidacaoDadosDeEntradaPrato.Mensagem);
+            // Passo 1.2 desenvolver método para validar os parametros de categoria -> já é validado na entrada do prato na categoria
             // Passo 1.3: Validar se o prato e a categoria que foram recebidos já existem no cardápio
             var pratoConsultado = _adsAplicacaoDPratos.ConsultarPratoPorNomeECategoria(parametroPrato.PratoNome, parametroPrato.CategoriaNome);
             if(pratoConsultado is not null)
                 return BadRequest("Este prato já existe");
-            // Passo 1.4: 
+            // Passo 1.4: Inserir prato dentro da categoria no cardápio
+            _adsAplicacaoDPratos.InserirPratoNaCategoria(parametroPrato.PratoNome, parametroPrato.CategoriaNome);
 
             return Ok();
         }
--- ADS.Delivery/obj/Debug/net8.0/ADS.Delivery.API.V1.AssemblyInfo.cs
+++ ADS.Delivery/obj/Debug/net8.0/ADS.Delivery.API.V1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ADS.Delivery.API.V1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2b360e2a925dd24786dd93dbb6eb54432f4377b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89ecf5d18df81296e8c27e6df62fa20642fa5971")]
 [assembly: System.Reflection.AssemblyProductAttribute("ADS.Delivery.API.V1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ADS.Delivery.API.V1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
