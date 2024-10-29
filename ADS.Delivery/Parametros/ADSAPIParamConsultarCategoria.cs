37885b3a02c6fc16e9e42b6e577d743d7010d4e5
AkiraShirou <87336992+AkiraShirou@users.noreply.github.com>
2024-10-28 23:14:11 -0300

    perf: criaçao da DTO ConsultarCategoria DTO criada para Consultar uma categoria com sua lista de pratos

---
+++ ADS.Delivery/Parametros/ADSAPIParamConsultarCategoria.cs
@@ -0,0 +1,7 @@
+﻿namespace ADS.Delivery.API.V1.Parametros;
+
+public class ADSAPIParamConsultarCategoria
+{
+    public string CategoriaNome { get; set; } = string.Empty;
+    public List<ADSDAPIParamInserirPrato> Pratos { get; set; } = new List<ADSDAPIParamInserirPrato>();
+}
