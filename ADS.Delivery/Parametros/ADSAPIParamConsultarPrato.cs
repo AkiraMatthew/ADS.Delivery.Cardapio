0179f6021319c2cf86436f0423b85afac32cb60f
AkiraShirou <87336992+AkiraShirou@users.noreply.github.com>
2024-10-28 23:14:52 -0300

    perf: criaçao da DTO ConsultarPrato DTO criada para Consultar um prato e sua categoria

---
+++ ADS.Delivery/Parametros/ADSAPIParamConsultarPrato.cs
@@ -0,0 +1,9 @@
+﻿namespace ADS.Delivery.API.V1.Parametros;
+
+public class ADSDAPIParamConsultarPrato
+{
+    public string PratoNome { get; set; } = string.Empty;
+    public string PratoDescricao { get; set; } = string.Empty;
+    public decimal PratoPreco { get; set; } = 0;
+    public string CategoriaNome { get; set; } = string.Empty; 
+}
