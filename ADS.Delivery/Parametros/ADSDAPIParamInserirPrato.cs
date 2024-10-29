817a0299d3bfeb0d33333c8b95710921cfbc7e44
AkiraShirou <87336992+AkiraShirou@users.noreply.github.com>
2024-10-28 23:30:51 -0300

    refactor: update das classes de inserir parametros

--- ADS.Delivery/Parametros/ADSDAPIParamInserirPrato.cs
+++ ADS.Delivery/Parametros/ADSDAPIParamInserirPrato.cs
@@ -6,10 +6,6 @@ public class ADSDAPIParamInserirPrato
     public string PratoNome { get; set; } = string.Empty;
     public string PratoDescricao { get; set; } = string.Empty;
     public decimal PratoPreco { get; set; } = 0;
-
-    /// TODO Este método precisa sair da API e é necessário adaptar as chamadas para chamar de dentro da API de categorias
-    /// O client nao precisa saber da relaçao entre as tabelas de Prato e Categoria, o service faz esse trabalho ao enviar os dados para o repositório
-    //public string CategoriaNome { get; set; } = string.Empty;
     #endregion
     public (bool Resultado, string? Mensagem) ValidarPropsDeEntradaPrato()
     {
